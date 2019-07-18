54a9a5ad6bc03ad13935ccf71b0ee8f289b2c9cc
Julio Calderon <juliansfreelance@gmail.com>
2019-07-18 10:41:28 -0500

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+
+export interface InfoPagina {
+  titulo?: string;
+  tm?: string;
+  email?: string;
+  nombre_corto?: string;
+  facebook?: string;
+  twitter?: string;
+  linkedin?: string;
+  instagram?: string;
+  equipo_trabajo?: any[];
+}
